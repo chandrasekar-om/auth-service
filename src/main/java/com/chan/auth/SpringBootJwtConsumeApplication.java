1c89d13bf27348fb9d6abb62248295d566bf2e34
Chandrasekar J <chandrasekarj@Chandrasekars-MacBook-Pro.local>
2020-11-05 15:11:47 +0530

    Auth-Service:Initial

---
+++ src/main/java/com/chan/auth/SpringBootJwtConsumeApplication.java
@@ -0,0 +1,14 @@
+package com.chan.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtConsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtConsumeApplication.class, args);
+    }
+
+
+}
